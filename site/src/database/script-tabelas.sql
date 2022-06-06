9f2ff43f356f287a507aa3ca28ab3ea94480eb5c
JoaoBrangel <joao.reis@sptech.school>
2022-06-05 21:53:12 -0300

    entregraveis

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -2,28 +2,25 @@
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 /* para workbench - local - desenvolvimento */
-CREATE DATABASE acquatec;
-
-USE acquatec;
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
-);
 
 CREATE DATABASE nudeMotel;
 
 USE nudemotel;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    email VARCHAR(50),
     rg varchar(12),
     dataNasc date,
-	senha VARCHAR(50)
+    senha VARCHAR(50),
+    canalP varchar(40)
+);
+
+create table suite (
+    id int primary key auto_increment,
+    nome varchar(50),
+    numero int,
+    tipo varchar(50)
 );
 select * from usuario;
-drop database nudeMotel;
